e3dc1cf8b7235de52dc1de8058c77c6dfaf30436
Lia Skalkos <hello@liaskalkos.net>
2015-07-14 15:51:58 -0400

    One refactor

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,10 @@
 class CommentsController < ApplicationController
 
   def create
-    @comment = Comment.new(comment_params)
-    @comment.post_id = params[:post_id]
-    @comment.save
+    @post = Post.find(params[:post_id])
+    @post.comments.create(comment_params)
 
-    redirect_to post_path(@comment.post)
+    redirect_to post_path(@post)
   end
 
   private
